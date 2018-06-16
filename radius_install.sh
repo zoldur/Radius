b176c1b5f289dc42f9cfefe2c9d2e7bfb88e0cc2
Zoldur <zoldur99@gmail.com>
2018-06-16 03:20:16 -0700

    Get IP

--- radius_install.sh
+++ radius_install.sh
@@ -12,7 +12,7 @@ COIN_NAME='Radius'
 COIN_PORT=4090
 RPC_PORT=4089
 
-NODEIP=$(curl -s4 icanhazip.com)
+NODEIP=$(curl -s4 api.ipify.org)
 
 
 RED='\033[0;31m'
@@ -147,7 +147,7 @@ function get_ip() {
   declare -a NODE_IPS
   for ips in $(netstat -i | awk '!/Kernel|Iface|lo/ {print $1," "}')
   do
-    NODE_IPS+=($(curl --interface $ips --connect-timeout 2 -s4 icanhazip.com))
+    NODE_IPS+=($(curl --interface $ips --connect-timeout 2 -s4 api.ipify.org))
   done
 
   if [ ${#NODE_IPS[@]} -gt 1 ]
